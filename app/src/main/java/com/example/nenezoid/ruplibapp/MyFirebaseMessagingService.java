37eb183e6109beb5ed53d8dc2ec274c1193a221a
guygo <guygordani2@gmail.com>
2018-01-17 18:02:36 +0200

    change FiireBase Service

--- app/src/main/java/com/example/nenezoid/ruplibapp/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/nenezoid/ruplibapp/MyFirebaseMessagingService.java
@@ -5,11 +5,14 @@ package com.example.nenezoid.ruplibapp;
  */
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.media.RingtoneManager;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -33,19 +36,35 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage message) {
 
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(getApplicationContext(), "M_CH_ID");
+        NotificationCompat.Builder notificationBuilder ;
 
+
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String chanel_id = "3000";
+            CharSequence name = "Channel Name";
+            String description = "Chanel Description";
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel mChannel = new NotificationChannel(chanel_id, name, importance);
+            mChannel.setDescription(description);
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.BLUE);
+            notificationManager.createNotificationChannel(mChannel);
+            notificationBuilder = new NotificationCompat.Builder(this, chanel_id);
+        } else {
+            notificationBuilder = new NotificationCompat.Builder(this);
+        }
         notificationBuilder.setAutoCancel(true)
                 .setDefaults(Notification.DEFAULT_ALL)
                 .setWhen(System.currentTimeMillis())
                 .setTicker("Hearty365")
                 .setPriority(Notification.PRIORITY_MAX)
                 .setSmallIcon(R.drawable.ic_book) // this is deprecated in API 26 but you can still use for below 26. check below update for 26 API
-                .setContentTitle("Default notification")
-                .setContentText("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+                .setContentTitle(message.getNotification().getTitle())
+                .setContentText(message.getNotification().getBody())
                 .setContentInfo("Info");
 
-        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         notificationManager.notify(0, notificationBuilder.build());
 
