a6caba1b3e6887456d67493c599010d2f4049f0f
BearReaper <comp.engineer8@gmail.com>
2018-01-22 18:44:26 +0200

    unreg search fixed note: updated xml for show book - revert in case of failure

--- app/src/main/java/com/example/nenezoid/ruplibapp/ShowBookActivity.java
+++ app/src/main/java/com/example/nenezoid/ruplibapp/ShowBookActivity.java
@@ -1,15 +1,12 @@
 package com.example.nenezoid.ruplibapp;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
@@ -18,10 +15,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class ShowBookActivity extends AppCompatActivity {
 
@@ -49,11 +42,7 @@ public class ShowBookActivity extends AppCompatActivity {
             bookDescripation=b.getString("Returned Description");
             avi=b.getBoolean("Returned Bool");
             bookurl=b.getString("bookurl");
-
         }
-
-
-
         TextView title_tv = findViewById(R.id.titleTextView);
         title_tv.setText("Title: "+strTitle);
         TextView author_tv = findViewById(R.id.authorTextView);
@@ -69,18 +58,16 @@ public class ShowBookActivity extends AppCompatActivity {
         Button resetButton=(Button)findViewById(R.id.orderButton);
         final FirebaseAuth mAuth= FirebaseAuth.getInstance();
         resetButton.setVisibility(View.GONE);
-        if(mAuth.getCurrentUser()!=null)
-        {
-            resetButton.setVisibility(View.VISIBLE);
-        }
         final boolean availble=avi;
         final String id=strTitle;
         final String strKeyy = bookKey;
-        final String userId=mAuth.getCurrentUser().getUid();
-
-        resetButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        if(mAuth.getCurrentUser()!=null)
+        {
+            resetButton.setVisibility(View.VISIBLE);
+            final String userId=mAuth.getCurrentUser().getUid();
+            resetButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
 
                     final FirebaseDatabase database = FirebaseDatabase.getInstance();
                     final DatabaseReference myRef = database.getReference("Books");
@@ -100,19 +87,19 @@ public class ShowBookActivity extends AppCompatActivity {
                             }
                         });
                     }
-                    else {
+                    else
                         userBookRef.child(id).child(userId).setValue("order");
 
+                    //Intent intent = new Intent(getApplicationContext(),ShowBookActivity.class);
+                    intent.putExtra("Returned Bool",false);
+                    startActivity(intent);
+                }
+            });
+        }
 
-                    }
 
 
 
-                //Intent intent = new Intent(getApplicationContext(),ShowBookActivity.class);
-                intent.putExtra("Returned Bool",false);
-                startActivity(intent);
-            }
-        });
 
         // Some function to fetch the info from firebase to page should go here
         // Example for link for pic https://firebasestorage.googleapis.com/v0/b/ruplibapp.appspot.com/o/net.jpg?alt=media&token=c837d082-f8c0-46f3-a18b-8877a57dcdc0
